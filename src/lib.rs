7e312a5c695187d264e90de53baf9c1a36a91be1
ManiGhazaee <hosseinghazaee1@gmail.com>
2024-01-28 14:45:04 +0330

    use jwalk

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 use std::{
-    fs::read_dir,
-    io::{self, StdoutLock, Write},
+    io::{self, stdout, Write},
     path::Path,
     sync::{Arc, Mutex},
 };
 
-use rayon::prelude::*;
 use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
 
 #[derive(Debug)]
@@ -14,98 +12,83 @@ pub struct Match<'a> {
     pub path: &'a [u8],
 }
 
-pub fn search_par_write(
-    path: &Path,
-    pat: &[u8],
-    pat_len: usize,
-    buf: &Arc<Mutex<termcolor::Buffer>>,
-) {
-    if path.is_dir() {
-        if let Ok(d) = read_dir(path) {
-            d.into_iter().into_iter().par_bridge().for_each(|entry| {
-                let path = match entry {
-                    Ok(e) => e.path(),
-                    _ => return,
-                };
-                search_par_write(&path, pat, pat_len, buf);
-
-                let path = path.to_str().unwrap_or("").as_bytes();
-                if pat_len > path.len() {
-                    return;
-                }
-                let mut col: Vec<usize> = Vec::new();
-                let mut i = 0;
-                while i <= path.len() - pat_len {
-                    if &path[i..i + pat_len] == pat {
-                        col.push(i + 1);
-                    }
-                    i += 1;
-                }
-                if col.is_empty() {
-                    return;
-                }
-                let _ = write_match(Match { col, path }, pat_len, buf);
-            });
+pub fn scan_print_end(path: &Path, pat: &[u8], pat_len: usize) {
+    let bw = BufferWriter::stdout(ColorChoice::Always);
+    let b = Arc::new(Mutex::new(bw.buffer()));
+    for entry in jwalk::WalkDir::new(Path::new(path)) {
+        let entry = match entry {
+            Ok(e) => e,
+            _ => continue,
+        };
+        let path = entry.path();
+        let path = path.to_str().unwrap_or("").as_bytes();
+        let path_len = path.len();
+        if pat_len > path_len {
+            continue;
         }
-    };
+        let mut col: Vec<usize> = Vec::new();
+        let mut i = 0;
+        while i <= path_len - pat_len {
+            if &path[i..i + pat_len] == pat {
+                col.push(i + 1);
+            }
+            i += 1;
+        }
+        if col.is_empty() {
+            continue;
+        }
+        let _ = write_match(Match { col, path }, pat_len, &b);
+    }
+    bw.print(&b.lock().unwrap()).unwrap();
 }
 
-pub fn search_par_print(path: &Path, pat: &[u8], pat_len: usize) {
-    if path.is_dir() {
-        if let Ok(d) = read_dir(path) {
-            d.into_iter().into_iter().par_bridge().for_each(|entry| {
-                let path = match entry {
-                    Ok(e) => e.path(),
-                    _ => return,
-                };
-                search_par_print(&path, pat, pat_len);
-
-                let path = path.to_str().unwrap_or("").as_bytes();
-                if pat_len > path.len() {
-                    return;
-                }
-                let mut col: Vec<usize> = Vec::new();
-                let mut i = 0;
-                while i <= path.len() - pat_len {
-                    if &path[i..i + pat_len] == pat {
-                        col.push(i + 1);
-                    }
-                    i += 1;
-                }
-                if col.is_empty() {
-                    return;
-                }
-                let _ = print_match(Match { col, path }, pat_len);
-            });
+pub fn scan_print(path: &Path, pat: &[u8], pat_len: usize) {
+    for entry in jwalk::WalkDir::new(Path::new(path)) {
+        let entry = match entry {
+            Ok(e) => e,
+            _ => continue,
+        };
+        let path = entry.path();
+        let path = path.to_str().unwrap_or("").as_bytes();
+        let path_len = path.len();
+        if pat_len > path_len {
+            continue;
         }
-    };
+        let mut col: Vec<usize> = Vec::new();
+        let mut i = 0;
+        while i <= path_len - pat_len {
+            if &path[i..i + pat_len] == pat {
+                col.push(i + 1);
+            }
+            i += 1;
+        }
+        if col.is_empty() {
+            continue;
+        }
+        let _ = print_match(Match { col, path }, pat_len);
+    }
 }
 
-pub fn print_all_paths(path: &Path, handle: &mut StdoutLock) {
-    if path.is_dir() {
-        let entries: Vec<_> = read_dir(path)
-            .unwrap()
-            .par_bridge()
-            .filter_map(|entry| entry.ok())
-            .collect();
-
-        entries.into_iter().for_each(|i| {
-            let path = i.path();
-            handle
-                .write(path.to_str().unwrap_or("").as_bytes())
-                .unwrap_or_default();
-            write!(handle, "\n").unwrap_or_default();
-            print_all_paths(&path, handle);
-        });
+pub fn print_all_paths(path: &Path) {
+    let mut handle = stdout().lock();
+    for entry in jwalk::WalkDir::new(Path::new(path)) {
+        let entry = match entry {
+            Ok(e) => e,
+            _ => continue,
+        };
+        let path = entry.path();
+        let path = match path.to_str() {
+            Some(p) => p,
+            _ => continue,
+        };
+        let _ = writeln!(handle, "{}", path);
     }
 }
 
 pub fn write_match(m: Match, pat_len: usize, b: &Arc<Mutex<termcolor::Buffer>>) -> io::Result<()> {
     let mut binding = ColorSpec::new();
     let red = binding.set_fg(Some(Color::Red));
-
     let mut b = b.lock().unwrap();
-
     let mut j = 0;
     for i in m.col.iter() {
         let col = i - 1;
@@ -124,10 +107,8 @@ pub fn write_match(m: Match, pat_len: usize, b: &Arc<Mutex<termcolor::Buffer>>) 
 pub fn print_match(m: Match, pat_len: usize) -> io::Result<()> {
     let mut binding = ColorSpec::new();
     let red = binding.set_fg(Some(Color::Red));
-
     let bw = BufferWriter::stdout(ColorChoice::Always);
     let mut b = bw.buffer();
-
     let mut j = 0;
     for i in m.col.iter() {
         let col = i - 1;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::{
-    env, io::stdout, path::Path, sync::{Arc, Mutex}, time::Instant
-};
+use std::{env, path::Path, time::Instant};
 
-use pscan::{print_all_paths, search_par_print, search_par_write};
-use termcolor::{BufferWriter, ColorChoice};
+use pscan::{print_all_paths, scan_print};
 
 fn main() {
     let inst = Instant::now();
@@ -12,21 +9,16 @@ fn main() {
         Some(path) => path,
         _ => "./",
     };
-    let pat = match args.get(1) {
-        Some(pat) if pat != "-" && !pat.is_empty() => pat.as_bytes(),
+    match args.get(1) {
+        Some(pat) if pat != "-" && !pat.is_empty() => {
+            let pat = pat.as_bytes();
+            let pat_len = pat.len();
+            scan_print(Path::new(path), pat, pat_len);
+        }
         _ => {
-            let mut handle = stdout().lock();
-            print_all_paths(Path::new(path), &mut handle);
-            return;
+            print_all_paths(Path::new(path));
         }
     };
-    let pat_len = pat.len();
 
-    // let bw = BufferWriter::stdout(ColorChoice::Always);
-    // let mut b = Arc::new(Mutex::new(bw.buffer()));
-    // search_par_write(Path::new(path), pat, pat_len, &mut b);
-    // bw.print(&b.lock().unwrap()).unwrap();
-
-    search_par_print(Path::new(path), pat, pat_len);
     println!("{}ms", inst.elapsed().as_millis());
 }
